4ec27d03b566ca75a6e8b76428d551b865cc012a
Seth Doubek <doubekseth@gmail.com>
2023-10-23 20:08:10 -0600

    Messed with settings

--- cytoscape-marll.js
+++ cytoscape-marll.js
@@ -416,7 +416,7 @@ exports.FDLayout = FDLayout;
 module.exports = Object.freeze({
 	animate: true, // whether to show the layout as it's running; special 'end' value makes the layout animate like a discrete layout
 	refresh: 10, // number of ticks per frame; higher is faster but more jerky
-	maxIterations: 5000, // max iterations before the layout will bail out
+	maxIterations: 10000, // max iterations before the layout will bail out
 	maxSimulationTime: 40000, // max length in ms to run the layout
 	ungrabifyWhileSimulating: false, // so you can't drag nodes during layout
 	fit: true, // on every layout reposition of nodes, fit the viewport
