13d52486da4dc1c0c53637331fcbf0b43683c92a
Seth Doubek <doubekseth@gmail.com>
2023-11-07 22:16:26 -0700

    Messed with configs

--- cytoscape-marll.js
+++ cytoscape-marll.js
@@ -520,7 +520,7 @@
           module.exports = Object.freeze({
             animate: true, // whether to show the layout as it's running; special 'end' value makes the layout animate like a discrete layout
             refresh: 10, // number of ticks per frame; higher is faster but more jerky
-            maxIterations: 1000, // max iterations before the layout will bail out
+            maxIterations: 5000, // max iterations before the layout will bail out
             maxSimulationTime: 40000, // max length in ms to run the layout
             ungrabifyWhileSimulating: false, // so you can't drag nodes during layout
             fit: true, // on every layout reposition of nodes, fit the viewport
